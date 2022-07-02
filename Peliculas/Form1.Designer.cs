85b8d6d5a242ca632b4239f0166cd6e937f8a06a
Anni <annis4nchez@gmail.com>
2022-07-01 23:17:34 -0400

    Form Projects

---
+++ Peliculas/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace AlgoritmosForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._director = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._escritor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Clasificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Productora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button2.Location = new System.Drawing.Point(338, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(325, 40);
+            this.button2.TabIndex = 50;
+            this.button2.Text = "Imprimir archivo de texto";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(298, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 49;
+            this.label7.Text = "Productora";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Clasificacion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Duracion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(440, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Genero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Escritor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Director";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Titulo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this._titulo,
+            this._director,
+            this._escritor,
+            this._genero,
+            this._Clasificacion,
+            this._Productora});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(637, 197);
+            this.dataGridView1.TabIndex = 42;
+            // 
+            // _titulo
+            // 
+            this._titulo.DataPropertyName = "titulo";
+            this._titulo.HeaderText = "Titulo";
+            this._titulo.Name = "_titulo";
+            // 
+            // _director
+            // 
+            this._director.DataPropertyName = "director";
+            this._director.HeaderText = "Director";
+            this._director.Name = "_director";
+            // 
+            // _escritor
+            // 
+            this._escritor.DataPropertyName = "escritor";
+            this._escritor.HeaderText = "Escritor";
+            this._escritor.Name = "_escritor";
+            // 
+            // _genero
+            // 
+            this._genero.DataPropertyName = "genero";
+            this._genero.HeaderText = "Genero";
+            this._genero.Name = "_genero";
+            // 
+            // _Clasificacion
+            // 
+            this._Clasificacion.DataPropertyName = "clasificacion";
+            this._Clasificacion.HeaderText = "Clasificacion";
+            this._Clasificacion.Name = "_Clasificacion";
+            // 
+            // _Productora
+            // 
+            this._Productora.DataPropertyName = "productora";
+            this._Productora.HeaderText = "Productora";
+            this._Productora.Name = "_Productora";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Location = new System.Drawing.Point(26, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 40);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(297, 100);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(124, 29);
+            this.textBox7.TabIndex = 40;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(162, 100);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(124, 29);
+            this.textBox6.TabIndex = 39;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(19, 100);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(124, 29);
+            this.textBox5.TabIndex = 38;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(439, 42);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 29);
+            this.textBox4.TabIndex = 37;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(297, 42);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 29);
+            this.textBox3.TabIndex = 36;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 42);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 29);
+            this.textBox2.TabIndex = 35;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 29);
+            this.textBox1.TabIndex = 34;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Location = new System.Drawing.Point(26, 229);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(637, 163);
+            this.groupBox1.TabIndex = 51;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar Pelicula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _director;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _escritor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _genero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Clasificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Productora;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Peliculas/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmosForms
+{
+    public partial class Form1 : Form
+    {
+        int index = 0;
+
+        public Form1[] Peliculas = new Form1[20];
+
+        public string titulo { get; set; }
+        public string director { get; set; }
+        public string escritor { get; set; }
+        public string genero { get; set; }
+        public string duracion { get; set; }
+        public string clasificacion { get; set; }
+        public string productora { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 pelicula = new Form1();
+            pelicula.titulo = textBox1.Text;
+            pelicula.director = textBox2.Text;
+            pelicula.escritor = textBox3.Text;
+            pelicula.genero = textBox4.Text;
+            pelicula.duracion = textBox5.Text;
+            pelicula.clasificacion = textBox6.Text;
+            pelicula.productora = textBox7.Text;
+
+           Peliculas[index] = pelicula;
+
+            dataGridView1.Rows.Add(
+            Peliculas[index].titulo,
+            Peliculas[index].director,
+            Peliculas[index].escritor,
+            Peliculas[index].genero,
+            Peliculas[index].duracion,
+            Peliculas[index].clasificacion,
+            Peliculas[index].productora);
+
+             index ++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("C:/Users/HP/OneDrive - Universidad Central del Este/Contenido y Asignaciones/9no Semestre/Optimización de Algoritmos/prueba.txt", false))
+            {
+                int i = 0;
+
+                foreach(var el in Peliculas)          
+                {
+                    writer.WriteLine(
+                        "Listado de peliculas \n\n"+
+                        $"Pelicula  {i + 1}:\n\n"
+                    + $"{el.titulo}\n"
+                    + $"{el.director}\n"
+                    + $"{el.escritor}\n"
+                    + $"{el.genero}\n"
+                    + $"{el.duracion}\n"
+                    + $"{el.clasificacion}\n"
+                    + $"{el.productora}\n"
+                    );
+
+                    i++;
+                }
+
+                writer.Close();
+            }
+        }
+    }
+}
